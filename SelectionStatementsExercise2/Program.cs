3d1d4c2c7083ace4a30390b8af07c10b6201e22f
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2024-02-19 14:03:14 -0600

    exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine ("What is your favorite school subject?");
+
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "english":
+                    Console.WriteLine("Thats a very interesting subject!");
+                    break;
+
+                    case "math":
+                    Console.WriteLine("Thats a very interesting subject!");
+                    break;
+
+                case "spanish":
+                    Console.WriteLine("Thats a very interesting subject!");
+                    break;
+
+                case "gym":
+                    Console.WriteLine("I bet you are a healthy person!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("Thats a very interesting subject!");
+                    break;
+                default:
+                    Console.WriteLine("I'm sure that's a cool subject!");
+                    break;
+            }
         }
+        
     }
 }
